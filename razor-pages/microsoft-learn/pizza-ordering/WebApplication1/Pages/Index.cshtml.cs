b2a6cf1a27f04a73b9634eadd22755cad85ded44
erlenthi <51139837+erlenthi@users.noreply.github.com>
2026-02-03 15:59:16 +0100

    Bootstrap experimenting. Pizza form initialized

--- razor-pages/microsoft-learn/pizza-ordering/WebApplication1/Pages/Index.cshtml.cs
+++ razor-pages/microsoft-learn/pizza-ordering/WebApplication1/Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Pages;
 
 public class IndexModel : PageModel
 {
+        private readonly PizzaService _service;
+    public IList<Pizza> PizzaList { get;set; } = default!;
+
+    public IndexModel(PizzaService service)
+    {
+        _service = service;
+    }
     public void OnGet()
     {
-
+         PizzaList = _service.GetPizzas();
     }
 }
---
+++ razor-pages/microsoft-learn/pizza-ordering/WebApplication1/Pages/PizzaForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class PizzaFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
