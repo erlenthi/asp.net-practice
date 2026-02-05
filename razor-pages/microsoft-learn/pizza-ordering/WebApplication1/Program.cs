bedf311d6e237956716360c61e17bf69821deae4
erlenthi <51139837+erlenthi@users.noreply.github.com>
2026-02-05 16:20:30 +0100

    Add pizza names in javascript and pause button for progress bar

--- razor-pages/microsoft-learn/pizza-ordering/WebApplication1/Program.cs
+++ razor-pages/microsoft-learn/pizza-ordering/WebApplication1/Program.cs
@@ -19,31 +19,102 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<PizzaContext>();
-    
+
     if (!context.Pizzas.Any())
     {
         context.Pizzas.AddRange(
-            new Pizza 
-            { 
-                Name = "Margherita", 
-                Size = PizzaSize.Medium, 
-                IsGlutenFree = false, 
-                Price = 12.99M 
+            new Pizza
+            {
+                Name = "Margherita",
+                Size = PizzaSize.Medium,
+                IsGlutenFree = false,
+                Price = 12.99M
             },
-            new Pizza 
-            { 
-                Name = "Pepperoni", 
-                Size = PizzaSize.Large, 
-                IsGlutenFree = false, 
-                Price = 15.99M 
+            new Pizza
+            {
+                Name = "Pepperoni",
+                Size = PizzaSize.Large,
+                IsGlutenFree = false,
+                Price = 15.99M
             },
-            new Pizza 
-            { 
-                Name = "Veggie Delight", 
-                Size = PizzaSize.Medium, 
-                IsGlutenFree = true, 
-                Price = 13.99M 
+            new Pizza
+            {
+                Name = "Veggie Delight",
+                Size = PizzaSize.Medium,
+                IsGlutenFree = true,
+                Price = 13.99M
             }
+            ,
+                        new Pizza
+                        {
+                            Name = "Hawaiian",
+                            Size = PizzaSize.Large,
+                            IsGlutenFree = false,
+                            Price = 14.99M
+                        },
+                        new Pizza
+                        {
+                            Name = "BBQ Chicken",
+                            Size = PizzaSize.Medium,
+                            IsGlutenFree = false,
+                            Price = 16.99M
+                        },
+                        new Pizza
+                        {
+                            Name = "Meat Lovers",
+                            Size = PizzaSize.Large,
+                            IsGlutenFree = false,
+                            Price = 17.99M
+                        },
+                        new Pizza
+                        {
+                            Name = "Four Cheese",
+                            Size = PizzaSize.Small,
+                            IsGlutenFree = false,
+                            Price = 11.99M
+                        },
+                        new Pizza
+                        {
+                            Name = "Supreme",
+                            Size = PizzaSize.Large,
+                            IsGlutenFree = false,
+                            Price = 18.99M
+                        },
+                        new Pizza
+                        {
+                            Name = "Mediterranean",
+                            Size = PizzaSize.Medium,
+                            IsGlutenFree = true,
+                            Price = 15.49M
+                        },
+                        new Pizza
+                        {
+                            Name = "Buffalo Chicken",
+                            Size = PizzaSize.Medium,
+                            IsGlutenFree = false,
+                            Price = 16.49M
+                        },
+                        new Pizza
+                        {
+                            Name = "Mushroom Truffle",
+                            Size = PizzaSize.Small,
+                            IsGlutenFree = true,
+                            Price = 14.49M
+                        },
+                        new Pizza
+                        {
+                            Name = "Spicy Italian",
+                            Size = PizzaSize.Large,
+                            IsGlutenFree = false,
+                            Price = 16.99M
+                        },
+                        new Pizza
+                        {
+                            Name = "Garden Fresh",
+                            Size = PizzaSize.Medium,
+                            IsGlutenFree = true,
+                            Price = 13.49M
+                        }
         );
         context.SaveChanges();
     }
